0d6016f31db4053fbc932506784e995d04709cbc
Alexander Holmström <alexander.holmstro3@learnet.se>
2019-09-27 11:19:18 +0200

    2019-09-27

---
+++ Uppgift_3_1.java
@@ -0,0 +1,13 @@
+/*
+Övning 3.1 i boken
+2019-09-27
+Programmerare Alexander Holmström
+*/
+import javax.swing.*;
+public class Uppgift_3_1{
+	public static void main (String [] args){
+		
+
+
+	}
+}
---
+++ Uppgift_3_3.java
@@ -0,0 +1,20 @@
+/*
+Övning 3.3 i boken
+2019-09-27
+Programmerare Alexander Holmström
+*/
+import javax.swing.*;
+public class Uppgift_3_3{
+	public static void main (String [] args){
+		double a = 3,1415926536;
+		double b;
+		double c;
+		double d = 2;
+		double e;
+		c= Math.pow(b,d)
+		e= (d*a)
+		b = JOptionPane.showInputDialog("Radien");
+		JOptionPane.showMessageDialog(null, c);
+
+	}
+}
